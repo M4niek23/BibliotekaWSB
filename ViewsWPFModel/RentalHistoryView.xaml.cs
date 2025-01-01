995194753f7fd4c535a66f1a434f79a8c1cc8a59
Patryk Manka <63506786+M4niek23@users.noreply.github.com>
2025-01-01 21:58:38 +0100

    Dodanie BookSearchView.xaml,ChangePasswordWindows.xaml, ForgotPasswordWindow.xaml, MyAccountView.xaml,RegisterView.xaml,RentalHistoryView.xaml

---
+++ ViewsWPFModel/RentalHistoryView.xaml.cs
@@ -0,0 +1,90 @@
+﻿
+using BibliotekaWSB.Data;
+using BibliotekaWSB.Models;
+using BibliotekaWSB.Services;
+using BibliotekaWSB.ViewsWPFModel;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace BibliotekaWSB.ViewsWPFModel;
+
+public partial class RentalHistoryView : Window
+{
+    private readonly User _loggedUser;
+    private readonly RentalService _rentalService;
+
+    public RentalHistoryView(User user)
+    {
+        InitializeComponent();
+        _loggedUser = user;
+
+        var bookRepository = new FileBookRepository();
+        var rentalRepository = new FileRentalRepository();
+        _rentalService = new RentalService(bookRepository, rentalRepository);
+
+        LoadRentals();
+    }
+
+    private void LoadRentals()
+    {
+        var rentals = _rentalService.GetUserRentals(_loggedUser);
+        RentalList.ItemsSource = rentals; // Lista RentalViewModel
+    }
+
+    private void ReturnBook_Click(object sender, RoutedEventArgs e)
+    {
+        if (sender is Button button && button.DataContext is RentalViewModel rentalVM)
+        {
+            var rental = _rentalService.GetRentalById(rentalVM.RentalId);
+            if (rental != null)
+            {
+                _rentalService.ReturnBook(rental);
+                LoadRentals(); // Odświeżenie listy po zwrocie
+            }
+        }
+    }
+    public class NullToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value == null ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    private void btn_Back_Click(object sender, RoutedEventArgs e)
+    {
+        var mainWindow = new MainWindow(_loggedUser);
+        mainWindow.Show();
+        Close();
+    }
+
+    private void btnClose_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private void btnMinimize_Click(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
+    }
+    private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.LeftButton == MouseButtonState.Pressed)
+        {
+            DragMove();
+        }
+    }
+
+    private void RentalList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+
+    }
+}
--- obj/Debug/net8.0-windows/BibliotekaWSB.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/BibliotekaWSB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BibliotekaWSB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c261711947e7baff92a93dece3fb37857d0eb2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1afd9e730cca5729e02ffcdeb75818b85c571a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BibliotekaWSB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BibliotekaWSB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
